f70aa147fbd93d09325a55e068d62229fca6a0bd
Sethmichael01 <sethmichael1.sm@gmail.com>
2021-01-08 21:01:32 +0100

    Init

---
+++ WebViewApp/WebViewApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using WebViewApp.ViewModels;
+using WebViewApp.Views;
+using Xamarin.Forms;
+
+namespace WebViewApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
